76c2d2d46d7bb358744d81cde0604ed9f98e4bfc
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-29 20:40:49 +0800

    edited local repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// CONFIG IMPORTS
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+// PAGE IMPORTS
+// import Chat from './pages/Chat';
+
+// COMPONENT IMPORTS
+// import Flash from './components/Flash';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
