068d170789b98e1941a6a59e531785c71a626f6c
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-01 16:04:34 +0800

    edited navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,14 +2,34 @@
 import React from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 
+// ASSETS IMPORTS
+import logo from '../assets/logos/Disco-weather-logo.svg';
+
 const Navigation = () => {
+  const location = useLocation();
+  const navigate = useNavigate()
+
+  const refreshPage = () => {
+    if (location.pathname === '/') {
+      window.location.reload();
+    } else {
+      navigate("/");
+    }
+  }
+
   return (
-    <div className="navigation">
-      <NavLink exact="true" to="/" className="navlink">Home</NavLink>
-      <NavLink exact="true" to="/current-weather" className="navlink">Current weather</NavLink>
-      <NavLink exact="true" to="/forecast" className="navlink">Forecast</NavLink>
-      <NavLink exact="true" to="/sun-moon-times" className="navlink">Sunrise/Sunset times</NavLink>
-      <NavLink exact="true" to="/tide-tables" className="navlink">Tide tables</NavLink>
+    <div className="navigation d-flex justify-content-between">
+      <div className="d-flex align-items-center">
+        <img className="logo" src={logo} alt="MP logo" onClick={refreshPage}/>
+        <h3 className="ms-3 mb-0">Disco Weather</h3>
+      </div>
+      <div className="d-flex flex-row justify-content-between">
+        <NavLink exact="true" to="/" className="navlink">Home</NavLink>
+        <NavLink exact="true" to="/current-weather" className="navlink">Current weather</NavLink>
+        <NavLink exact="true" to="/forecast" className="navlink">Forecast</NavLink>
+        <NavLink exact="true" to="/sun-moon-times" className="navlink">Sunrise/Sunset times</NavLink>
+        <NavLink exact="true" to="/tide-tables" className="navlink">Tide tables</NavLink>
+      </div>
     </div>
   );
 };
---
+++ src/components/NavigationMobile.jsx.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavigationMobile = () => {
+  return (
+    <div className="nav-menu-card py-3 pointer">
+      <NavLink exact="true" to={navElement.navlink} className="navlink">
+        <div className="card-content p-2">
+          <img src={navElement.img} alt={navElement.title + " icon"} className="illustration align-self-center" />
+          <h4 className="fw-bold text-center mb-0 pt-2">{navElement.title}</h4>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default NavigationMobile;
