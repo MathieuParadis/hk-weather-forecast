03dfd35d6f20b5a829bf59255f6ed647bc74f166
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-02 10:44:35 +0800

    created weather forecast card component

---
+++ src/components/WeatherForecastCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeatherForecastCard = () => {
+  return (
+    <div className="weather-forecast-card">
+      
+    </div>
+  );
+};
+
+export default WeatherForecastCard;
--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -1,11 +1,15 @@
 // CONFIG IMPORTS
 import React, {useEffect, useState} from 'react';
 
+// COMPONENTS IMPORTS
+import WeatherForecastCard from '../components/WeatherForecastCard';
+
 const Forecast = () => {
   const [weatherForecastData, setWeatherForecastData] = useState('');
   const getWeatherForecastData = () => {
     const url = 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en';
     const icon_url = 'https://www.hko.gov.hk/images/HKOWxIconOutline/pic53.png';
+    const psr_icon_url = 'https://www.hko.gov.hk/common/images/PSRHigh_50.png';
 
     fetch(url, {
       method: "GET",
@@ -31,6 +35,22 @@ const Forecast = () => {
   return (
     <div className="forecast">
       <h1>Forecast page</h1>
+      <div>
+        <h3>General situation</h3>
+        <p>{weatherForecastData.generalSituation}</p>
+      </div>
+
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mb-4">
+        { weatherForecastData !== '' &&
+          weatherForecastData.rainfall.data.map((element) => {
+            return(
+              <WeatherForecastCard data={element} key={element.place} />
+            )
+          })
+        }
+      </div>
+
+
     </div>
   );
 };
