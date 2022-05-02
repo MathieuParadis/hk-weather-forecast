729f16169662561568d232d3c31f151488ca7d02
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-02 17:34:56 +0800

    edited forecastCard Component

--- src/components/WeatherForecastCard.jsx
+++ src/components/WeatherForecastCard.jsx
@@ -18,18 +18,16 @@ const WeatherForecastCard = ({data}) => {
 
   return (
     <div className="weather-forecast-card">
-      <div className="card-content d-flex flex-column justify-content-center align-items-center p-2 border-radius-5">
+      <div className="card-content d-flex flex-column justify-content-center align-items-center p-3 border-radius-5">
         <h5 className="text-center">{data.forecastDate}</h5>
         <h5 className="text-center">{data.week}</h5>
         <img src={`${weather_icon_url}${data.ForecastIcon}.png`} alt="weather forecast icon" className="weather-forecast-icon"/>
-        <div className="temp-section d-flex align-items-center">
-          <img src={temp_icon} alt="temperature icon" className="temperature-icon"/>
-          <p className="m-0">{data.forecastMintemp.value} - {data.forecastMaxtemp.value} C</p>
-        </div>
+        <p>{data.forecastMintemp.value} - {data.forecastMaxtemp.value} C</p>
         <p>Humidity: {data.forecastMinrh.value} - {data.forecastMaxrh.value}</p>
-        <img src={`${psr_icon_url}${formatPSR(data.PSR)}_50.png`} alt="PSR icon" className="PSR-icon"/>
-        <p>{data.PSR}</p>
-
+        <div className="temp-section d-flex align-items-center">
+          <img src={`${psr_icon_url}${formatPSR(data.PSR)}_50.png`} alt="PSR icon" className="PSR-icon"/>
+          <p className="m-0">{data.PSR}</p>
+        </div>
       </div>
     </div>
   );
