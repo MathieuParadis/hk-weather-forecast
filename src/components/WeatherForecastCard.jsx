0c34e3476dda1e02024f4b57da6995d1a3842760
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-02 12:20:59 +0800

    edited forecastCard Component

--- src/components/WeatherForecastCard.jsx
+++ src/components/WeatherForecastCard.jsx
@@ -4,13 +4,17 @@ const WeatherForecastCard = ({data}) => {
   const weather_icon_url = 'https://www.hko.gov.hk/images/HKOWxIconOutline/pic';
   const psr_icon_url = 'https://www.hko.gov.hk/common/images/PSR';
 
+  const url = "https://www.hko.gov.hk/textonly/v2/explain/wxicon_e.htm";
+
+
   return (
     <div className="weather-forecast-card">
       <div className="card-content p-3 border-radius-5">
         <h5 className="text-center">{data.forecastDate}</h5>
         <h5 className="text-center">{data.week}</h5>
         <img src={`${weather_icon_url}${data.ForecastIcon}.png` } alt="PSR icon" />
-
+        <p>Temperature: {data.forecastMintemp.value} - {data.forecastMaxtemp.value}</p>
+        <p>Humidity: {data.forecastMinrh.value} - {data.forecastMaxrh.value}</p>
         <img src={`${psr_icon_url}${data.PSR}_50.png` } alt="PSR icon" />
 
       </div>
