60c9f5c2cd00adbae17075fcc2fa5a393feff4f4
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-02 10:34:41 +0800

    edited forecast page

--- src/pages/CurrentWeather.jsx
+++ src/pages/CurrentWeather.jsx
@@ -34,11 +34,13 @@ const CurrentWeather = () => {
   return (
     <div className="current-weather">
       <h1>Current weather page</h1>
+
+      
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mb-4">
         { weatherData !== '' &&
           weatherData.rainfall.data.map((element) => {
             return(
-              <WeatherDistrictCard data={element} key={element.rainfall.data.place} />
+              <WeatherDistrictCard data={element} key={element.place} />
             )
           })
         }
--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -1,9 +1,31 @@
 // CONFIG IMPORTS
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Forecast = () => {
-  useEffect(() => {
+  const [weatherForecastData, setWeatherForecastData] = useState('');
+  const getWeatherForecastData = () => {
+    const url = 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en';
+    const icon_url = 'https://www.hko.gov.hk/images/HKOWxIconOutline/pic53.png';
+
+    fetch(url, {
+      method: "GET",
+    })
+    .then(res => res.json())
+    .catch(error => {
+        console.error('Error:', error);
+    })
+    .then(response => {
+        console.log(response);
+        setWeatherForecastData(response);
+    });
+  }
+
+    useEffect(() => {
     window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    getWeatherForecastData();
   }, []);
 
   return (
@@ -14,3 +36,5 @@ const Forecast = () => {
 };
 
 export default Forecast;
+
+
