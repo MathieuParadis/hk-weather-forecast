60eb747aa61608b84ee7d822e44a0a4ebe7ed20c
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-03 16:11:01 +0800

    edired SunMoon page

--- src/pages/SunMoon.jsx
+++ src/pages/SunMoon.jsx
@@ -14,13 +14,26 @@ const SunMoon = () => {
     fetch(file)
     .then(response => response.text())
     .then(response => {
-        let data = Papa.parse(response);
-        setParsedCsvData(data.data);
-        return data.data.shift()
+      let data = Papa.parse(response);
+      const obj = []
+
+      data.data.shift();
+      data.data.map((el) => {
+        const objEntry = {
+          "date": el[0],
+          "rise": el[1],
+          "tran": el[2],
+          "set": el[3],
+        }
+
+        obj.push(objEntry);
+      })
+
+      console.log(obj);
     });
   }
 
-  parseFile('Sun_rise_set_2018_2024.csv');
+  parseFile('Sun_rise_set_2018.csv');
 
   useEffect(() => {
     window.scrollTo(0, 0);
