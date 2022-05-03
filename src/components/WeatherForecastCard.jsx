0c832d9b221164f9905bc7322dc8430240449579
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-03 09:19:03 +0800

    edited forecastCard Component

--- src/components/WeatherForecastCard.jsx
+++ src/components/WeatherForecastCard.jsx
@@ -22,16 +22,22 @@ const WeatherForecastCard = ({data}) => {
   }
 
   return (
-    <div className="weather-forecast-card">
-      <div className="card-content d-flex flex-column justify-content-center align-items-center p-3 border-radius-5">
-        <h3 className="text-center">{formatDate(data.forecastDate)}</h3>
-        <p className="text-center h5">{data.week}</p>
-        <img src={`${weather_icon_url}${data.ForecastIcon}.png`} alt="weather forecast icon" className="weather-forecast-icon"/>
-        <p className="h5 fw-bold">{data.forecastMintemp.value} - {data.forecastMaxtemp.value} °C</p>
-        <p className="h5 fw-bold">{data.forecastMinrh.value} - {data.forecastMaxrh.value} %</p>
-        <div className="temp-section d-flex align-items-center">
-          <img src={`${psr_icon_url}${formatPSR(data.PSR)}_50.png`} alt="PSR icon" className="PSR-icon"/>&nbsp;&nbsp;
-          <p className="m-0">{data.PSR}</p>
+    <div className="weather-forecast-card pointer">
+      <div className="card-content border-radius-5">
+        <div className="d-flex flex-column justify-content-center align-items-center p-3">
+          <h3 className="text-center">{formatDate(data.forecastDate)}</h3>
+          <p className="text-center h5">{data.week}</p>
+          <img src={`${weather_icon_url}${data.ForecastIcon}.png`} alt="weather forecast icon" className="weather-forecast-icon"/>
+          <p className="h5 fw-bold">{data.forecastMintemp.value} - {data.forecastMaxtemp.value} °C</p>
+          <p className="h5 fw-bold">{data.forecastMinrh.value} - {data.forecastMaxrh.value} %</p>
+          <div className="temp-section d-flex align-items-center">
+            <img src={`${psr_icon_url}${formatPSR(data.PSR)}_50.png`} alt="PSR icon" className="PSR-icon"/>&nbsp;&nbsp;
+            <p className="m-0">{data.PSR}</p>
+          </div>
+        </div>
+        <div className="overlay"></div>
+        <div className="weather-info">
+          <h3 className="text-center">{data.forecastWeather}</h3>
         </div>
       </div>
     </div>
